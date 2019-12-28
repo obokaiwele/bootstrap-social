065c577de5a5a52113822e85265aa5f68a1743d1
@bokaiwele <https://github.com/obokaiwele>
2019-12-28 03:22:09 -0500

    Update docs with Bootstrap 4

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,19 +1,36 @@
 $(function () {
-  var all_classes = "";
-  var timer = undefined;
-  $.each($('li', '.social-class'), function (index, element) {
-    all_classes += " btn-" + $(element).data("code");
+  let allClasses = [];
+  let $socialClass = $('.social-class li');
+  let $sizesButton = $('.social-sizes a.btn-block');
+  let $socialButton = $('.social-sizes .btn');
+  let $socialIcon = $('.social-sizes .btn-social-icon');
+
+  $socialClass.each((index, element) => {
+    allClasses.push('btn-' + $(element).data('code'));
   });
-  $('li', '.social-class').mouseenter(function () {
-    var icon_name = $(this).data("code");
-    if ($(this).data("icon")) {
-      icon_name = $(this).data("icon");
+  addClasses = allClasses.join(' ');
+
+  $socialClass.mouseenter((event) => {
+    const $target = $(event.target);
+    const iconName = $target.data('icon') || $target.data('code');
+    if ( ! iconName ) {
+        return;
     }
-    var icon = "<i class='fa fa-" + icon_name + "'></i>";
-    $('.btn-social', '.social-sizes').html(icon + "Sign in with " + $(this).data("name"));
-    $('.btn-social-icon', '.social-sizes').html(icon);
-    $('.btn', '.social-sizes').removeClass(all_classes);
-    $('.btn', '.social-sizes').addClass("btn-" + $(this).data('code'));
+
+    let icon = "<i class='fa fa-" + iconName + "'></i>";
+
+    let targetName = $target.data('name');
+    if ( ! targetName ) {
+        console.log($target);
+    }
+    $sizesButton.html(icon + ' Sign in with ' + targetName);
+
+    $socialIcon.html(icon);
+    $socialButton.removeClass(allClasses);
+    $socialButton.addClass('btn-' + $target.data('code'));
   });
-  $($('li', '.social-class')[Math.floor($('li', '.social-class').length * Math.random())]).mouseenter();
+
+  // Initialize to a random selection
+  const index = Math.floor($socialClass.length * Math.random());
+  $( $socialClass[index] ).mouseenter();
 });
